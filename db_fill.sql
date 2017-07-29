6fe7086104627ef0f3c13c79b39a642eb7a817aa
scarlet-nerve <snehalbhagat95@gmail.com>
2017-07-30 02:20:50 +0530

    CHANGES

--- db_fill.sql
+++ db_fill.sql
@@ -1,6 +1,27 @@
-insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (499118665241 ,"Snehal M.", "8167328533","F", 1995-11-16);
-insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (499118665242 ,"Rushika M.", "8167328534","F", 1996-11-16);
-insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (499118665243 ,"Karishma M.", "8167328535","F", 1997-11-16);
-insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (499118665244 ,"Anurag M.", "8167328536","M", 1998-11-16);
-insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (499118665245 ,"Surmeet M.", "8167328537","M", 1991-11-16);
-insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (499118665246 ,"Miloy M.", "8167328538","F", 1992-11-16);
+insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (9161246 ,"Snehal M.", "8167328533",'M', 1995-11-16);
+insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (12911842 ,"Rushika M.", "8167328534",'M', 1996-11-16);
+insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (39984243 ,"Karishma M.", "8167328535",'M', 1997-11-16);
+insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (49911864 ,"Anurag M.", "8167328536",'M', 1998-11-16);
+insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (69915244 ,"Surmeet M.", "8167328537",'M', 1991-11-16);
+insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (99986645 ,"Miloy M.", "8167328538",'M', 1992-11-16);
+
+
+
+
+insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (49911866 ,"Ryan M.", "8167312538",'M', 1980-11-16);
+insert into table aadhar(aadhar_id, name, mobile, gender, dob, address) values (495246 ,"Webster M.", "9167328538",'M', 1978-11-16);
+
+
+
+
+insert into person values (9161246,"abcd",'Y','N','N');
+insert into person values (12911842,"1244",'N','N','Y');
+insert into person values (39984243,"1244",'N','N','Y');
+insert into person values (49911864,"1244",'N','N','Y');
+
+
+
+insert into child values (49911866,"Lilavati");
+insert into child values (495246,"Tata Memorial");
+
+
