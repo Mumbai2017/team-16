f0ab4c48209af4386aa1032c9455d92620ced467
Karishma Gajula <karishmajobs31@gmail.com>
2017-07-30 02:53:33 +0530

    json done for register

--- App/makeAwish/app/src/main/java/good/force/teah/makeawish/Registration.java
+++ App/makeAwish/app/src/main/java/good/force/teah/makeawish/Registration.java
@@ -3,19 +3,25 @@ package good.force.teah.makeawish;
 import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Calendar;
 import java.util.Date;
 
 public class Registration extends AppCompatActivity {
 
-    private EditText name,aadhar,dateOfBirth;
-    private Button submit,pickDate;
-    private int mYear, mMonth, mDay;
+    private EditText aadhar,password;
+    private Button register;
+    private CheckBox dr, vol, donor;
 
 
 
@@ -24,42 +30,50 @@ public class Registration extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        name = (EditText)findViewById(R.id.name);
         aadhar = (EditText)findViewById(R.id.aadhar);
-        dateOfBirth = (EditText)findViewById(R.id.dateOfBirth);
-        submit = (Button)findViewById(R.id.button_submit);
-        pickDate = (Button) findViewById(R.id.button_pickDate);
+       password=(EditText) findViewById(R.id.password);
+        register=(Button) findViewById(R.id.register);
+        dr=(CheckBox) findViewById(R.id.checkBox2);
+        vol=(CheckBox) findViewById(R.id.checkBox3);
+        donor=(CheckBox) findViewById(R.id.checkBox4);
 
-        submit.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO submit data
-
+                if(aadhar.getText().toString().length()==0 || password.getText().toString().length()==0 )
+                {
+                    Toast.makeText(getApplicationContext(),"Please enter all details",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    JSONObject obj=new JSONObject();
+                    try
+                    {
+                        obj.put("aadharNo",aadhar.getText().toString());
+                        obj.put("password",password.getText().toString());
+                        if(dr.isChecked())
+                            obj.put("doctor","y");
+                        else
+                            obj.put("doctor","n");
+                        if(vol.isChecked())
+                            obj.put("volunteer","y");
+                        else
+                            obj.put("volunteer","n");
+                        if(donor.isChecked())
+                            obj.put("donor","y");
+                        else
+                            obj.put("donor","n");
+                        Log.d("json",obj.toString());
+                    }
+                    catch (JSONException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
 
-        pickDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Get Current Date
-                final Calendar c = Calendar.getInstance();
-                mYear = c.get(Calendar.YEAR);
-                mMonth = c.get(Calendar.MONTH);
-                mDay = c.get(Calendar.DAY_OF_MONTH);
-                DatePickerDialog datePickerDialog = new DatePickerDialog(getApplicationContext(),
-                        new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
 
-                                dateOfBirth.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
-
-                            }
-                        }, mYear, mMonth, mDay);
-                datePickerDialog.show();
-
-            }
-        });
 
 
     }
