9d0b3446b21dd0a8e1ab39547a3073586d6f0738
DeeJay-NuKe <ask222g@gmail.com>
2017-07-29 22:44:44 +0530

    data strcutures for client

--- App/makeAwish/app/src/main/java/good/force/teah/makeawish/Data/Doctor.java
+++ App/makeAwish/app/src/main/java/good/force/teah/makeawish/Data/Doctor.java
@@ -5,5 +5,67 @@ package good.force.teah.makeawish.Data;
  */
 
 public class Doctor {
+    int aadharID;
+    String name;
+    String address;
+    String hospital;
+    String specialization;
+    int type;
+    String gender;
 
+    public int getAadhar_id() {
+        return aadharID;
+    }
+
+    public void setAadhar_id(int aadhar_id) {
+        this.aadharID = aadhar_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(String hospital) {
+        this.hospital = hospital;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
--- App/makeAwish/app/src/main/java/good/force/teah/makeawish/Data/Volunteer.java
+++ App/makeAwish/app/src/main/java/good/force/teah/makeawish/Data/Volunteer.java
@@ -5,4 +5,58 @@ package good.force.teah.makeawish.Data;
  */
 
 public class Volunteer {
+    int aadharID;
+    String name;
+    String address;
+    int type;
+    String gender;
+    String dateOfBirth;
+
+    public int getAadharID() {
+        return aadharID;
+    }
+
+    public void setAadharID(int aadharID) {
+        this.aadharID = aadharID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
