5fec6281ef4050300cc8bbf2d6c04a4064d9a087
Surmeet <surmeetkaur.95@gmail.com>
2017-07-30 03:32:41 +0530

    homescreen and corresponding activities completed

--- App/makeAwish/app/src/main/java/good/force/teah/makeawish/Data/wish.java
+++ App/makeAwish/app/src/main/java/good/force/teah/makeawish/Data/wish.java
@@ -1,43 +1,10 @@
 package good.force.teah.makeawish.Data;
 
 /**
- * Created by Sarbjit on 30-07-2017.
+ * Created by Anurag on 7/30/2017.
  */
 
-public class wish {
-        private String title, genre, year;
-
-        public wish() {
-        }
-
-        public wish(String title, String genre, String year) {
-            this.title = title;
-            this.genre = genre;
-            this.year = year;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String name) {
-            this.title = name;
-        }
-
-        public String getYear() {
-            return year;
-        }
-
-        public void setYear(String year) {
-            this.year = year;
-        }
-
-        public String getGenre() {
-            return genre;
-        }
-
-        public void setGenre(String genre) {
-            this.genre = genre;
-        }
-    }
-
+public class Wish {
+    private String type;
+    //private
+}
