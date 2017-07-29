a2b39313bffeffba681092ec745aeb62b63db4f0
Karishma Gajula <karishmajobs31@gmail.com>
2017-07-29 23:37:03 +0530

    merge changes

--- App/makeAwish/app/src/main/java/good/force/teah/makeawish/Registration.java
+++ App/makeAwish/app/src/main/java/good/force/teah/makeawish/Registration.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -13,9 +14,9 @@ import java.util.Date;
 
 public class Registration extends AppCompatActivity {
 
-    private EditText name,aadhar,dateOfBirth;
-    private Button submit,pickDate;
-    private int mYear, mMonth, mDay;
+    private EditText aadhar, password;
+    private Button register;
+    private CheckBox doctor, vol, donor;
 
 
 
@@ -23,43 +24,26 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+aadhar=(EditText) findViewById(R.id.aadhar);
+        password=(EditText) findViewById(R.id.password);
+        register=(Button) findViewById(R.id.register);
 
-        name = (EditText)findViewById(R.id.name);
-        aadhar = (EditText)findViewById(R.id.aadhar);
-        dateOfBirth = (EditText)findViewById(R.id.dateOfBirth);
-        submit = (Button)findViewById(R.id.button_submit);
-        pickDate = (Button) findViewById(R.id.button_pickDate);
+        doctor=(CheckBox) findViewById(R.id.checkBox2) ;
+        vol=(CheckBox) findViewById(R.id.checkBox3);
+        donor=(CheckBox) findViewById(R.id.checkBox4);
 
-        submit.setOnClickListener(new View.OnClickListener() {
+
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO submit data
 
             }
         });
 
-        pickDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Get Current Date
-                final Calendar c = Calendar.getInstance();
-                mYear = c.get(Calendar.YEAR);
-                mMonth = c.get(Calendar.MONTH);
-                mDay = c.get(Calendar.DAY_OF_MONTH);
-                DatePickerDialog datePickerDialog = new DatePickerDialog(getApplicationContext(),
-                        new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
+       
 
-                                dateOfBirth.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
 
-                            }
-                        }, mYear, mMonth, mDay);
-                datePickerDialog.show();
 
-            }
-        });
 
 
     }
