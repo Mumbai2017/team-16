bb9fbdd6ff9ac69430ee4d0c71332033ec75af4a
DeeJay-NuKe <ask222g@gmail.com>
2017-07-29 22:00:46 +0530

    Add datePicker

--- App/makeAwish/app/src/main/java/good/force/teah/makeawish/Registration.java
+++ App/makeAwish/app/src/main/java/good/force/teah/makeawish/Registration.java
@@ -1,17 +1,22 @@
 package good.force.teah.makeawish;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class Registration extends AppCompatActivity {
 
-    EditText name,aadhar,dateOfBirth;
-    Button submit;
+    private EditText name,aadhar,dateOfBirth;
+    private Button submit,pickDate;
+    private int mYear, mMonth, mDay;
+
 
 
     @Override
@@ -23,17 +28,38 @@ public class Registration extends AppCompatActivity {
         aadhar = (EditText)findViewById(R.id.aadhar);
         dateOfBirth = (EditText)findViewById(R.id.dateOfBirth);
         submit = (Button)findViewById(R.id.button_submit);
+        pickDate = (Button) findViewById(R.id.button_pickDate);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //TODO submit data
 
             }
         });
 
+        pickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get Current Date
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getApplicationContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
 
+                                dateOfBirth.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
 
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
 
+            }
+        });
 
 
     }
