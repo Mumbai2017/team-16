95c6205c3932a5171c17d93424248a6339b72530
Surmeet <surmeetkaur.95@gmail.com>
2017-07-30 00:49:40 +0530

    navigation drawer initiated

--- App/makeAwish/app/src/main/java/good/force/teah/makeawish/homescreen.java
+++ App/makeAwish/app/src/main/java/good/force/teah/makeawish/homescreen.java
@@ -8,86 +8,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import static good.force.teah.makeawish.R.styleable.View;
 
 public class homescreen extends AppCompatActivity {
 
-    private String[] mPlanetTitles;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
+    ImageButton viewProfile, viewReferrals , addReferrals, faq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
-        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        viewProfile = (ImageButton)findViewById(R.id.button_profile);
+        viewReferrals = (ImageButton)findViewById(R.id.button_view_refferal);
+        addReferrals = (ImageButton)findViewById(R.id.button_add_refferal);
+        faq = (ImageButton)findViewById(R.id.button_faq);
 
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mPlanetTitles));
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-        /** Called when a drawer has settled in a completely closed state. */
-        public void onDrawerClosed(View view) {
-            super.onDrawerClosed(view);
-            getActionBar().setTitle(mTitle);
-            invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-        }
 
-        /** Called when a drawer has settled in a completely open state. */
-        public void onDrawerOpened(View drawerView) {
-            super.onDrawerOpened(drawerView);
-            getActionBar().setTitle(mDrawerTitle);
-            invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-        }
 
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-    }
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
-
-    /** Swaps fragments in the main content view */
-    private void selectItem(int position) {
-        // Create a new fragment and specify the planet to show based on position
-        Fragment fragment = new PlanetFragment();
-        Bundle args = new Bundle();
-        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-        fragment.setArguments(args);
-
-        // Insert the fragment by replacing any existing fragment
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.content_frame, fragment)
-                .commit();
-
-        // Highlight the selected item, update the title, and close the drawer
-        mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
-    }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
     }
 }
