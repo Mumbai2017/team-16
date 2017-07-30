ba6c0fb9e927457e0a8e92d22166f40e1007ebdd
Surmeet <surmeetkaur.95@gmail.com>
2017-07-30 09:29:12 +0530

    profile corrected

--- App/makeAwish/app/src/main/java/good/force/teah/makeawish/profile.java
+++ App/makeAwish/app/src/main/java/good/force/teah/makeawish/profile.java
@@ -10,9 +10,11 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -24,7 +26,7 @@ import java.util.concurrent.ExecutionException;
 public class profile extends AppCompatActivity {
 
     mySyncTask syncTask;
-    EditText blah;
+    TextView nameOfInsti,aadhar,lat,lon, age,name ;
    // Bundle bundle=new Bundle();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +36,13 @@ public class profile extends AppCompatActivity {
         Intent out = getIntent();
         Log.i("FLAG","hey2");
 
-        blah = (EditText)findViewById(R.id.editText5);
+        nameOfInsti = (TextView)findViewById(R.id.instText);
+        aadhar = (TextView)findViewById(R.id.aadharText);
+        lat = (TextView)findViewById(R.id.latText);
+        lon = (TextView)findViewById(R.id.lonText);
+        age = (TextView)findViewById(R.id.ageText);
+        name = (TextView)findViewById(R.id.nameText);
+
 
         syncTask=new mySyncTask();
         Log.i("FLAG","hey3");
@@ -44,21 +52,10 @@ public class profile extends AppCompatActivity {
           //  syncTask.execute(out.getStringExtra("state"),out.getStringExtra("cat"),out.getStringExtra("data"));
 
         ArrayList<String> xlist=new ArrayList<>();
-        String listString = "";
+
         try
         {
             xlist = syncTask.get();
-
-
-
-            for (String s : xlist)
-            {
-                listString += s + "\t";
-            }
-            //if(xlist!=null)
-           Log.i("list",listString);
-            //else
-            //  Toast.makeText(this,"Error,xlist is empty",Toast.LENGTH_LONG);
         }
         catch (InterruptedException e)
         {
@@ -68,10 +65,13 @@ public class profile extends AppCompatActivity {
         {
             e.printStackTrace();
         }
-       blah.setText(listString);
-       // bundle.putStringArrayList("list",xlist);
 
-
+        nameOfInsti.setText(xlist.get(0));
+        lat.setText(xlist.get(0));
+        lon.setText(xlist.get(0));
+        aadhar.setText("-------");
+        age.setText("21");
+        name.setText("Gul");
     }
 
 }
@@ -83,8 +83,6 @@ class mySyncTask extends AsyncTask<String, Void, ArrayList>
     @Override
     protected ArrayList doInBackground(String... params)
     {
-
-        Log.i("BLAH","inside class");
         String state=params[0];
         String index=null,data=null;
         if(params.length>1)
@@ -93,8 +91,6 @@ class mySyncTask extends AsyncTask<String, Void, ArrayList>
             data=params[2];
         }
 
-//        Log.i("state: ",state);
-        Log.i("BLAH","inside class");
         String API_KEY = "854b4425ebca54132fbe0c337b5afbbf";
         try
         {
@@ -105,16 +101,12 @@ class mySyncTask extends AsyncTask<String, Void, ArrayList>
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             try
             {
-                Log.i("BLAH","inside second try catch");
-
                 ArrayList<String> list=new ArrayList<>();
                 BufferedReader reader=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
                 JSONObject jsonObject=new JSONObject(reader.readLine());
                 JSONArray jsonArray=jsonObject.getJSONArray("data");
                 JSONArray jsonArray1=jsonArray.getJSONObject(0).getJSONArray("practices");
                 JSONObject jsonObject2 = jsonArray1.getJSONObject(0);
-                Log.i("BLAH111",jsonObject2.get("name").toString());
-                Log.i("BLAH",jsonArray.toString());
 
                 if (index!=null)
                 {
@@ -123,12 +115,9 @@ class mySyncTask extends AsyncTask<String, Void, ArrayList>
 
                 Log.i("blah",jsonArray.toString());
 
-              // JSONArray jsonObject1 = jsonArray.getJSONObject(0).get("practices");
-
                 list.add(jsonObject2.get("name").toString());
-                //list.add(jsonArray.getJSONObject(0).get("lat").toString());
-                //list.add(jsonArray.getJSONObject(0).get("ParaContractTeachersMale").toString());
-                //list.add(jsonArray.getJSONObject(0).get("ParaContractTeachersFemale").toString());
+                list.add(jsonObject2.get("lat").toString());
+                list.add(jsonObject2.get("lon").toString());
 
                 Log.i("Array list in MySyncTa",jsonArray.toString());
 
