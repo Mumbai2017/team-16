25f832d2b12984aa71f0982b6fedd5a6ad5fcd31
Karishma Gajula <karishmajobs31@gmail.com>
2017-07-30 06:31:22 +0530

    homescreen and recyclerview upated

--- App/makeAwish/app/src/main/java/good/force/teah/makeawish/Data/wishAdapter.java
+++ App/makeAwish/app/src/main/java/good/force/teah/makeawish/Data/wishAdapter.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 import java.util.List;
 
 import good.force.teah.makeawish.R;
-/*
+
 public class wishAdapter extends RecyclerView.Adapter<wishAdapter.MyViewHolder> {
 
     private List<wish> listData;
@@ -71,4 +71,3 @@ public class wishAdapter extends RecyclerView.Adapter<wishAdapter.MyViewHolder> 
     }
     }
 
-*/
